dd7209d77c2e1d2508ecc485f47361cfc5ec8222
猫吉 <freeaddress834@gmail.com>
2021-03-08 17:08:12 +0900

    Genericのinterface, それを継承したclassを定義

--- GenericPractice.java
+++ GenericPractice.java
@@ -1,15 +1,41 @@
 public class GenericPractice {
     public static void main(String[] args) {
-        
+        GenericClass2<Integer, String> genericClass2 = new GenericClass2<>();
+        genericClass2.set(20, "20");
+        System.out.println(genericClass2.getKey());
+        System.out.println(genericClass2.getValue());
     }
 }
 
-public class GenericClass1<T> {
+class GenericClass1<T> {
     private T t;
     public void set(T t) {
         this.t = t;
     }
     public T get() {
         return t;
+    }
+}
+
+public interface GenericInterface<K,V> {
+    public K getKey();
+    public V getValue();
+}
+
+class GenericClass2<K,V> implements GenericInterface<K,V> {
+    private K k;
+    private V v;
+
+    public void set(K k, V v) {
+        this.k = k;
+        this.v = v;
+    }
+
+    public K getKey() {
+        return k;
+    }
+
+    public V getValue() {
+        return v;
     }
 }
