e76c1a26a8f2c5e8081c6a9d90e745b769055453
猫吉 <freeaddress834@gmail.com>
2021-03-08 18:52:24 +0900

    ジェネリクスとサブタイプについて

--- GenericPractice.java
+++ GenericPractice.java
@@ -2,17 +2,19 @@ public class GenericPractice {
     public static void main(String[] args) {
         GenericClass1<Integer> class1 = new GenericClass1<>();
         GenericClass1<Integer> class2 = new GenericClass1<>();
-        class1.set("100");
+        class1.set(100);
         class2.set(100);
         // GenericClass2<Integer, Integer> genericClass2 = new GenericClass2<>(20, 100);
         // System.out.println(rawbox.getKey());
-        compare(class1, class2);
+        class1.compare(100);
     }
 
     public static <K,V> void compare(GenericClass2<K,V> t1, GenericClass2<K,V> t2) {
     }
 }
-
+ 
+// 境界（extendsでデータ型の型を制限）
+// IntegerはNumberのサブクラス
 class GenericClass1<T extends Number> {
     private T t;
     public void set(T t) {
@@ -20,6 +22,13 @@ class GenericClass1<T extends Number> {
     }
     public T get() {
         return t;
+    }
+    public boolean compare(T otherT) {
+        if (this.t == otherT) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
