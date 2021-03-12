4312925bd3f57d1e80acce2d479f358f2a277506
猫吉 <freeaddress834@gmail.com>
2021-03-12 17:34:18 +0900

    komennto

--- ExceptionPractice.java
+++ ExceptionPractice.java
@@ -2,13 +2,13 @@ public class ExceptionPractice {
     public static void main(String[] args) {
         int a = 10;
         int b = 0;
-        try {
+        try { // 例外処理の開始
             int c = a / b;
             System.out.println("処理の成功");
-        } catch (Exception e) {
+        } catch (Exception e) { // 例外時に通過
             //TODO: handle exception
             System.out.println("例外をキャッチしました" + e);
-        } finally {
+        } finally { // 例外にかかわらず通過
             System.out.println("通過");
         }
         System.out.println("プログラムを終了");
