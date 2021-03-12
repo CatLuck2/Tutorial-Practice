c2e7169e5112d48ac57ba276e6491dfbf88af03d
猫吉 <freeaddress834@gmail.com>
2021-03-12 18:45:15 +0900

    スレッドを一時停止

--- ThreadPractice.java
+++ ThreadPractice.java
@@ -11,6 +11,19 @@ public class ThreadPractice {
     public static void main(String[] args) {
         MyThread thread1 = new MyThread();
         Thread thread2 = new Thread(thread1); // Runnableクラスを引数にする
-        thread2.start(); // Threadクラスのrun()を呼び出す?
+        // thread2.start(); // Threadクラスのrun()を呼び出す
+
+        ThreadMethod1();
+    }
+    
+    static void ThreadMethod1() {
+        for (int i = 0; i < 20; i++) {
+            try {
+                Thread.sleep(5000); // 5000ミリ秒（5秒）停止
+            } catch (InterruptedException e) { // スレッドで割り込みが発生した時
+                System.out.println(e);
+            }
+            System.out.println("end");
+        }
     }
 }
