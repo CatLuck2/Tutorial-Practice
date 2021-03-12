dbf8401d053d35e9ac20ca95a0b2d43ef9a4242e
猫吉 <freeaddress834@gmail.com>
2021-03-12 17:33:21 +0900

    try-catch-finallyを記述

--- ExceptionPractice.java
+++ ExceptionPractice.java
@@ -3,11 +3,13 @@ public class ExceptionPractice {
         int a = 10;
         int b = 0;
         try {
-            String c = a + b;
+            int c = a / b;
             System.out.println("処理の成功");
         } catch (Exception e) {
             //TODO: handle exception
-            System.out.println("例外をキャッチしました");
+            System.out.println("例外をキャッチしました" + e);
+        } finally {
+            System.out.println("通過");
         }
         System.out.println("プログラムを終了");
     }
