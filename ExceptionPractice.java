7f2c5eceb9611a8b81e8b9d2dc4cbc9814b49a0a
猫吉 <freeaddress834@gmail.com>
2021-03-12 17:44:51 +0900

    複数の例外を記述

--- ExceptionPractice.java
+++ ExceptionPractice.java
@@ -1,14 +1,21 @@
+/*
+複数の例外パターンをcatchで記述できる
+*/
+
 public class ExceptionPractice {
     public static void main(String[] args) {
         int array[] = new int[10]; 
         try { // 例外処理の開始
             array[99] = 11;
             System.out.println("処理の成功");
-        } catch (Exception e) { // 例外時に通過
+        } catch (ArithmeticException e) { // 例外時に通過
             //TODO: handle exception
+            System.out.println("AccessException");
             e.printStackTrace();
             System.out.println(e.getMessage());
-            System.out.println(e.getCause());
+            System.out.println(e.getLocalizedMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException");
         } finally { // 例外にかかわらず通過
             System.out.println("通過");
         }
