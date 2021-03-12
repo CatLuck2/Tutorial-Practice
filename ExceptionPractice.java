b09b5923a77545300aa8f63d6b16358e0fa6ea04
猫吉 <freeaddress834@gmail.com>
2021-03-12 17:37:46 +0900

    配列の例外処理、エラーのプロパティを試した

--- ExceptionPractice.java
+++ ExceptionPractice.java
@@ -1,13 +1,14 @@
 public class ExceptionPractice {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 0;
+        int array[] = new int[10]; 
         try { // 例外処理の開始
-            int c = a / b;
+            array[99] = 11;
             System.out.println("処理の成功");
         } catch (Exception e) { // 例外時に通過
             //TODO: handle exception
-            System.out.println("例外をキャッチしました" + e);
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            System.out.println(e.getCause());
         } finally { // 例外にかかわらず通過
             System.out.println("通過");
         }
