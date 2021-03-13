dd56cec0ace908c877a2c3d4e19ee7f9f3b31a3b
猫吉 <freeaddress834@gmail.com>
2021-03-14 08:25:31 +0900

    ファイルを読み込み

--- InputPractice.java
+++ InputPractice.java
@@ -2,7 +2,8 @@ import java.io.*;
 
 public class InputPractice {
 	public static void main(String[] args) {
-        InputFile("/Users/fuji_yosu/Desktop/test.txt");
+        // InputFile("/Users/ユーザ名/Desktop/test.txt");
+        ReadFile("/Users/fuji_yosu/Desktop/test.txt");
     }
 
     static void InputMethod() {
@@ -33,6 +34,28 @@ public class InputPractice {
 				bw.write("[" + i + "]");
 			}
             bw.close();
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+
+    static void ReadFile(String filePath) {
+        try {
+            // 読み込み先のパス
+            File file = new File(filePath);
+            // 指定先を読み込み
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String s;
+            // 1行目を読み込む
+            // s = br.readLine();
+            // System.out.println(s);
+
+            // 文字（スペースも含む）のある行を読み込む
+            while ((s = br.readLine()) != null) {
+                System.out.println(s);
+            }
+            br.close();
         } catch (Exception e) {
             //TODO: handle exception
         }
