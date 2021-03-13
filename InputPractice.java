78602504d4624d9700382184db9c4c86608ce870
猫吉 <freeaddress834@gmail.com>
2021-03-14 00:10:30 +0900

    文字列を入力し、出力

---
+++ InputPractice.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class InputPractice {
+	public static void main(String[] args) {
+        // 入力を受け取る
+        InputStreamReader in = new InputStreamReader(System.in);
+        // 受け取った入力とプログラムの橋渡しをする
+        BufferedReader reader = new BufferedReader(in);
+        try {
+            // 入力値から文字列データをString型で取得
+            String string = reader.readLine();
+            System.out.println("入力値は" + string);
+            reader.close();
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e);
+        }
+    }
+}
