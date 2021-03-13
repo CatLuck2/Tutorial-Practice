e6a4aaf2cf0c3cb4a01c5897401f1273b4e4432e
猫吉 <freeaddress834@gmail.com>
2021-03-13 22:25:42 +0900

    内部クラスを修正

--- RamudaPractice.java
+++ RamudaPractice.java
@@ -1,19 +1,25 @@
-public class RamudaPractice {
-    public static void main(String[] args) {
-        Outer outer = new Outer();
-        outer.doSomething();
-    }
+// メソッドが1つの時は関数型インターフェースと呼ぶ
+interface HelloWorld {
+    public void hello();
 }
 
-class Outer {
-    void doSomething() {
+public class RamudaPractice {
+    public static void main(String[] args) {
         // メソッド内でクラスを定義
-        class Inner {
-            void print() {
+        class Inner implements HelloWorld {
+            public void hello() {
+                System.out.println("HelloWorld");
+            }
+            public void print() {
                 System.out.println("Innerクラスのprintメソッド");
             }
         }
         Inner inner = new Inner();
-        inner.print();
+        greeting(inner);
     }
+
+    static void greeting(HelloWorld h) {
+        h.hello();
+        System.out.println("Nice to meet you");
+    } 
 }
