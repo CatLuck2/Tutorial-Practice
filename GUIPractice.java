85f53069e417562d828f79f11483079d4cfb1cf4
猫吉 <freeaddress834@gmail.com>
2021-03-14 09:40:18 +0900

    Frameをクラスで定義

--- GUIPractice.java
+++ GUIPractice.java
@@ -2,12 +2,28 @@ import javax.swing.JFrame;
 
 public class GUIPractice {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        // これがないと終了できない
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // サイズ指定
-		frame.setSize(500, 200);
-        // フレームを表示
-		frame.setVisible(true);
+        // JFrame frame = new JFrame();
+        // // これがないと終了できない
+		// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // // サイズ指定
+		// frame.setSize(500, 200);
+        // // フレームを表示
+		// frame.setVisible(true);
+
+        JFremeMethod1();
+    }
+
+    static void JFremeMethod1() {
+        // Frameの定義
+        class MyFrame extends JFrame {
+            // Frameのコンストラクタ
+            MyFrame() {
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setSize(200, 300);
+                setVisible(true);
+            }
+        }
+        // 画面の構築
+        new MyFrame();
     }
 }
